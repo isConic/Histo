afa0d7813036c0a0e01e9e3c16c9f09720601e0a
isConic <conicrelief@gmail.com>
2019-12-03 17:55:39 -0500

    Add config_file template. Add example of google places api usage.

---
+++ histogram_scraping.py
@@ -0,0 +1,22 @@
+import yaml
+import googlemaps
+from datetime import datetime
+
+configs = yaml.load(open("configs/google_api.yaml", "r"))
+client_key = configs["client_key"]
+print(client_key)
+
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
